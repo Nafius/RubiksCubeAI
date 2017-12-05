c757b199fd810f744fce8c96321864bb06dea3df
JoshuaAugustus <32554470+JoshuaAugustus@users.noreply.github.com>
2017-12-05 01:23:34 -0600

    Added new AI manipulation methods
    Created additional methods to return a new, manipulated Cube object for use with recursion.

--- rubik.cs
+++ rubik.cs
@@ -67,6 +67,20 @@ class Cube{
 		this.solvePath.Clear();
 	}
 	
+	public Cube(Cube clone){
+		for(int i = 0; i < 9; i++){
+			this.top.getSquare(i).setColor(clone.top.getSquare(i).getColor());
+			this.bottom.getSquare(i).setColor(clone.bottom.getSquare(i).getColor());
+			this.front.getSquare(i).setColor(clone.front.getSquare(i).getColor());
+			this.back.getSquare(i).setColor(clone.back.getSquare(i).getColor());
+			this.left.getSquare(i).setColor(clone.left.getSquare(i).getColor());
+			this.right.getSquare(i).setColor(clone.right.getSquare(i).getColor());
+		}
+		
+		for(int j = 0; j < clone.getSolvePath().Count; j++){
+			this.solvePath.Add(clone.getSolvePath()[j]);
+		}
+	}
 	/*
 	* Randomly shuffles the Cube object <sAmount> times.
 	* Will only shuffle clockwise to avoid redundancy.
@@ -397,6 +411,333 @@ class Cube{
 		this.solvePath.Add(11);
 	}
 	
+	public Cube createLeftClockwise(){
+		Cube newCube = this;
+		
+		newCube.holder.getSquare(0).setColor(newCube.front.getSquare(0).getColor());
+		newCube.holder.getSquare(3).setColor(newCube.front.getSquare(3).getColor());
+		newCube.holder.getSquare(6).setColor(newCube.front.getSquare(6).getColor());
+		
+		newCube.front.getSquare(0).setColor(newCube.top.getSquare(0).getColor());
+		newCube.front.getSquare(3).setColor(newCube.top.getSquare(3).getColor());
+		newCube.front.getSquare(6).setColor(newCube.top.getSquare(6).getColor());
+		
+		newCube.top.getSquare(0).setColor(newCube.back.getSquare(8).getColor());
+		newCube.top.getSquare(3).setColor(newCube.back.getSquare(5).getColor());
+		newCube.top.getSquare(6).setColor(newCube.back.getSquare(2).getColor());
+		
+		newCube.back.getSquare(2).setColor(newCube.bottom.getSquare(6).getColor());
+		newCube.back.getSquare(5).setColor(newCube.bottom.getSquare(3).getColor());
+		newCube.back.getSquare(8).setColor(newCube.bottom.getSquare(0).getColor());
+		
+		newCube.bottom.getSquare(0).setColor(newCube.holder.getSquare(0).getColor());
+		newCube.bottom.getSquare(3).setColor(newCube.holder.getSquare(3).getColor());
+		newCube.bottom.getSquare(6).setColor(newCube.holder.getSquare(6).getColor());
+		
+		newCube.solvePath.Add(0);
+		
+		return newCube;
+	}
+	
+	/* 
+	* Shuffles the left/orange Side of the Cube object in a counter clockwise direction.
+	* Adds the corresponding path number to the Cube's solvePath list when finished.
+	*/
+	public Cube createLeftCounterClockwise() {
+		Cube newCube = this;
+		
+		newCube.holder.getSquare(0).setColor(newCube.front.getSquare(0).getColor());
+		newCube.holder.getSquare(3).setColor(newCube.front.getSquare(3).getColor());
+		newCube.holder.getSquare(6).setColor(newCube.front.getSquare(6).getColor());
+		
+		newCube.front.getSquare(0).setColor(newCube.bottom.getSquare(0).getColor());
+		newCube.front.getSquare(3).setColor(newCube.bottom.getSquare(3).getColor());
+		newCube.front.getSquare(6).setColor(newCube.bottom.getSquare(6).getColor());
+		
+		newCube.bottom.getSquare(0).setColor(newCube.back.getSquare(8).getColor());
+		newCube.bottom.getSquare(3).setColor(newCube.back.getSquare(5).getColor());
+		newCube.bottom.getSquare(6).setColor(newCube.back.getSquare(2).getColor());
+		
+		newCube.back.getSquare(2).setColor(newCube.top.getSquare(6).getColor());
+		newCube.back.getSquare(5).setColor(newCube.top.getSquare(3).getColor());
+		newCube.back.getSquare(8).setColor(newCube.top.getSquare(0).getColor());
+		
+		newCube.top.getSquare(0).setColor(newCube.holder.getSquare(0).getColor());
+		newCube.top.getSquare(3).setColor(newCube.holder.getSquare(3).getColor());
+		newCube.top.getSquare(6).setColor(newCube.holder.getSquare(6).getColor());
+		
+		newCube.solvePath.Add(1);
+		
+		return newCube;
+	}
+	
+	/* 
+	* Shuffles the right/red Side of the Cube object in a clockwise direction.
+	* Adds the corresponding path number to the Cube's solvePath list when finished.
+	*/
+	public Cube createRightClockwise() {
+		Cube newCube = this;
+		
+		newCube.holder.getSquare(2).setColor(newCube.front.getSquare(2).getColor());
+		newCube.holder.getSquare(5).setColor(newCube.front.getSquare(5).getColor());
+		newCube.holder.getSquare(8).setColor(newCube.front.getSquare(8).getColor());
+		
+		newCube.front.getSquare(2).setColor(newCube.bottom.getSquare(2).getColor());
+		newCube.front.getSquare(5).setColor(newCube.bottom.getSquare(5).getColor());
+		newCube.front.getSquare(8).setColor(newCube.bottom.getSquare(8).getColor());
+		
+		newCube.bottom.getSquare(2).setColor(newCube.back.getSquare(6).getColor());
+		newCube.bottom.getSquare(5).setColor(newCube.back.getSquare(3).getColor());
+		newCube.bottom.getSquare(8).setColor(newCube.back.getSquare(0).getColor());
+		
+		newCube.back.getSquare(0).setColor(newCube.top.getSquare(8).getColor());
+		newCube.back.getSquare(3).setColor(newCube.top.getSquare(5).getColor());
+		newCube.back.getSquare(6).setColor(newCube.top.getSquare(2).getColor());
+		
+		newCube.top.getSquare(2).setColor(newCube.holder.getSquare(2).getColor());
+		newCube.top.getSquare(5).setColor(newCube.holder.getSquare(5).getColor());
+		newCube.top.getSquare(8).setColor(newCube.holder.getSquare(8).getColor());
+
+		newCube.solvePath.Add(2);
+		
+		return newCube;
+	}
+	
+	/* 
+	* Shuffles the right/red Side of the Cube object in a counter clockwise direction.
+	* Adds the corresponding path number to the Cube's solvePath list when finished.
+	*/
+	public Cube createRightCounterClockwise() {
+		Cube newCube = this;
+		
+		newCube.holder.getSquare(2).setColor(newCube.front.getSquare(2).getColor());
+		newCube.holder.getSquare(5).setColor(newCube.front.getSquare(5).getColor());
+		newCube.holder.getSquare(8).setColor(newCube.front.getSquare(8).getColor());
+		
+		newCube.front.getSquare(2).setColor(newCube.top.getSquare(2).getColor());
+		newCube.front.getSquare(5).setColor(newCube.top.getSquare(5).getColor());
+		newCube.front.getSquare(8).setColor(newCube.top.getSquare(8).getColor());
+		
+		newCube.top.getSquare(2).setColor(newCube.back.getSquare(6).getColor());
+		newCube.top.getSquare(5).setColor(newCube.back.getSquare(3).getColor());
+		newCube.top.getSquare(8).setColor(newCube.back.getSquare(0).getColor());
+		
+		newCube.back.getSquare(0).setColor(newCube.bottom.getSquare(8).getColor());
+		newCube.back.getSquare(3).setColor(newCube.bottom.getSquare(5).getColor());
+		newCube.back.getSquare(6).setColor(newCube.bottom.getSquare(2).getColor());
+		
+		newCube.bottom.getSquare(2).setColor(newCube.holder.getSquare(2).getColor());
+		newCube.bottom.getSquare(5).setColor(newCube.holder.getSquare(5).getColor());
+		newCube.bottom.getSquare(8).setColor(newCube.holder.getSquare(8).getColor());
+		
+		newCube.solvePath.Add(3);
+		
+		return newCube;
+	}
+	
+	/* 
+	* Shuffles the top/white Side of the Cube object in a clockwise direction.
+	* Adds the corresponding path number to the Cube's solvePath list when finished.
+	*/
+	public Cube createTopClockwise() {
+		Cube newCube = this;
+		
+		for(int i = 0; i < 3; i++) {
+			newCube.holder.getSquare(i).setColor(newCube.front.getSquare(i).getColor());
+			newCube.front.getSquare(i).setColor(newCube.right.getSquare(i).getColor());
+			newCube.right.getSquare(i).setColor(newCube.back.getSquare(i).getColor());
+			newCube.back.getSquare(i).setColor(newCube.left.getSquare(i).getColor());
+			newCube.left.getSquare(i).setColor(newCube.holder.getSquare(i).getColor());
+		}
+		newCube.solvePath.Add(4);
+		
+		return newCube;
+	}
+	
+	/* 
+	* Shuffles the top/white Side of the Cube object in a counter clockwise direction.
+	* Adds the corresponding path number to the Cube's solvePath list when finished.
+	*/
+	public Cube createTopCounterClockwise() {
+		Cube newCube = this;
+		
+		for(int i = 0; i < 3; i++) {
+			newCube.holder.getSquare(i).setColor(newCube.front.getSquare(i).getColor());
+			newCube.front.getSquare(i).setColor(newCube.left.getSquare(i).getColor());
+			newCube.left.getSquare(i).setColor(newCube.back.getSquare(i).getColor());
+			newCube.back.getSquare(i).setColor(newCube.right.getSquare(i).getColor());
+			newCube.right.getSquare(i).setColor(newCube.holder.getSquare(i).getColor());
+		}
+		newCube.solvePath.Add(5);
+		
+		return newCube;
+	}
+	
+	/* 
+	* Shuffles the bottom/yellow Side of the Cube object in a clockwise direction.
+	* Adds the corresponding path number to the Cube's solvePath list when finished.
+	*/
+	public Cube createBottomClockwise() {
+		Cube newCube = this;
+		
+		for(int i = 6; i < 9; i++) {
+			newCube.holder.getSquare(i).setColor(newCube.front.getSquare(i).getColor());
+			newCube.front.getSquare(i).setColor(newCube.left.getSquare(i).getColor());
+			newCube.left.getSquare(i).setColor(newCube.back.getSquare(i).getColor());
+			newCube.back.getSquare(i).setColor(newCube.right.getSquare(i).getColor());
+			newCube.right.getSquare(i).setColor(newCube.holder.getSquare(i).getColor());
+		}
+		newCube.solvePath.Add(6);
+		
+		return newCube;
+	}
+	
+	/* 
+	* Shuffles the bottom/yellow Side of the Cube object in a count clockwise direction.
+	* Adds the corresponding path number to the Cube's solvePath list when finished.
+	*/
+	public Cube createBottomCounterClockwise() {
+		Cube newCube = this;
+		
+		for(int i = 6; i < 9; i++) {
+			newCube.holder.getSquare(i).setColor(newCube.front.getSquare(i).getColor());
+			newCube.front.getSquare(i).setColor(newCube.right.getSquare(i).getColor());
+			newCube.right.getSquare(i).setColor(newCube.back.getSquare(i).getColor());
+			newCube.back.getSquare(i).setColor(newCube.left.getSquare(i).getColor());
+			newCube.left.getSquare(i).setColor(newCube.holder.getSquare(i).getColor());
+		}
+		newCube.solvePath.Add(7);
+		
+		return newCube;
+	}
+	
+	/* 
+	* Shuffles the front/green Side of the Cube object in a clockwise direction.
+	* Adds the corresponding path number to the Cube's solvePath list when finished.
+	*/
+	public Cube createFrontClockwise() {
+		Cube newCube = this;
+		
+		for(int i = 6; i < 9; i++) {
+			newCube.holder.getSquare(i).setColor(newCube.top.getSquare(i).getColor());
+		}
+		
+		newCube.top.getSquare(6).setColor(newCube.left.getSquare(8).getColor());
+		newCube.top.getSquare(7).setColor(newCube.left.getSquare(5).getColor());
+		newCube.top.getSquare(8).setColor(newCube.left.getSquare(2).getColor());
+		
+		newCube.left.getSquare(2).setColor(newCube.bottom.getSquare(0).getColor());
+		newCube.left.getSquare(5).setColor(newCube.bottom.getSquare(1).getColor());
+		newCube.left.getSquare(8).setColor(newCube.bottom.getSquare(2).getColor());
+		
+		newCube.bottom.getSquare(0).setColor(newCube.right.getSquare(6).getColor());
+		newCube.bottom.getSquare(1).setColor(newCube.right.getSquare(3).getColor());
+		newCube.bottom.getSquare(2).setColor(newCube.right.getSquare(0).getColor());
+		
+		newCube.right.getSquare(0).setColor(newCube.holder.getSquare(6).getColor());
+		newCube.right.getSquare(3).setColor(newCube.holder.getSquare(7).getColor());
+		newCube.right.getSquare(6).setColor(newCube.holder.getSquare(8).getColor());
+
+		newCube.solvePath.Add(8);
+		
+		return newCube;
+	}
+	
+	/* 
+	* Shuffles the front/green Side of the Cube object in a counter clockwise direction.
+	* Adds the corresponding path number to the Cube's solvePath list when finished.
+	*/
+	public Cube createFrontCounterClockwise() {
+		Cube newCube = this;
+		
+		for(int i = 6; i < 9; i++) {
+			newCube.holder.getSquare(i).setColor(newCube.top.getSquare(i).getColor());
+		}
+		
+		newCube.top.getSquare(6).setColor(newCube.right.getSquare(0).getColor());
+		newCube.top.getSquare(7).setColor(newCube.right.getSquare(3).getColor());
+		newCube.top.getSquare(8).setColor(newCube.right.getSquare(6).getColor());
+		
+		newCube.right.getSquare(0).setColor(newCube.bottom.getSquare(2).getColor());
+		newCube.right.getSquare(3).setColor(newCube.bottom.getSquare(1).getColor());
+		newCube.right.getSquare(6).setColor(newCube.bottom.getSquare(0).getColor());
+		
+		newCube.bottom.getSquare(0).setColor(newCube.left.getSquare(2).getColor());
+		newCube.bottom.getSquare(1).setColor(newCube.left.getSquare(5).getColor());
+		newCube.bottom.getSquare(2).setColor(newCube.left.getSquare(8).getColor());
+		
+		newCube.left.getSquare(2).setColor(newCube.holder.getSquare(8).getColor());
+		newCube.left.getSquare(5).setColor(newCube.holder.getSquare(7).getColor());
+		newCube.left.getSquare(8).setColor(newCube.holder.getSquare(6).getColor());
+		
+		newCube.solvePath.Add(9);
+		
+		return newCube;
+	}
+	
+	/* 
+	* Shuffles the back/blue Side of the Cube object in a clockwise direction.
+	* Adds the corresponding path number to the Cube's solvePath list when finished.
+	*/
+	public Cube createBackClockwise() {
+		Cube newCube = this;
+		
+		for(int i = 0; i < 3; i++) {
+			newCube.holder.getSquare(i).setColor(newCube.top.getSquare(i).getColor());
+		}
+		
+		newCube.top.getSquare(0).setColor(newCube.right.getSquare(2).getColor());
+		newCube.top.getSquare(1).setColor(newCube.right.getSquare(5).getColor());
+		newCube.top.getSquare(2).setColor(newCube.right.getSquare(8).getColor());
+
+		newCube.right.getSquare(2).setColor(newCube.bottom.getSquare(8).getColor());
+		newCube.right.getSquare(5).setColor(newCube.bottom.getSquare(7).getColor());
+		newCube.right.getSquare(8).setColor(newCube.bottom.getSquare(6).getColor());
+
+		newCube.bottom.getSquare(6).setColor(newCube.left.getSquare(0).getColor());
+		newCube.bottom.getSquare(7).setColor(newCube.left.getSquare(3).getColor());
+		newCube.bottom.getSquare(8).setColor(newCube.left.getSquare(6).getColor());
+
+		newCube.left.getSquare(0).setColor(newCube.holder.getSquare(2).getColor());
+		newCube.left.getSquare(3).setColor(newCube.holder.getSquare(1).getColor());
+		newCube.left.getSquare(6).setColor(newCube.holder.getSquare(0).getColor());
+		
+		newCube.solvePath.Add(10);
+		
+		return newCube;
+	}
+	
+	/* 
+	* Shuffles the back/blue Side of the Cube object in a counter clockwise direction.
+	* Adds the corresponding path number to the Cube's solvePath list when finished.
+	*/
+	public Cube createBackCounterClockwise() {
+		Cube newCube = this;
+		
+		for(int i = 0; i < 3; i++) {
+			newCube.holder.getSquare(i).setColor(newCube.top.getSquare(i).getColor());
+		}
+		
+		newCube.top.getSquare(0).setColor(newCube.left.getSquare(6).getColor());
+		newCube.top.getSquare(1).setColor(newCube.left.getSquare(3).getColor());
+		newCube.top.getSquare(2).setColor(newCube.left.getSquare(0).getColor());
+		
+		newCube.left.getSquare(0).setColor(newCube.bottom.getSquare(6).getColor());
+		newCube.left.getSquare(3).setColor(newCube.bottom.getSquare(7).getColor());
+		newCube.left.getSquare(6).setColor(newCube.bottom.getSquare(8).getColor());
+		
+		newCube.bottom.getSquare(6).setColor(newCube.right.getSquare(8).getColor());
+		newCube.bottom.getSquare(7).setColor(newCube.right.getSquare(5).getColor());
+		newCube.bottom.getSquare(8).setColor(newCube.right.getSquare(2).getColor());
+		
+		newCube.right.getSquare(2).setColor(newCube.holder.getSquare(0).getColor());
+		newCube.right.getSquare(5).setColor(newCube.holder.getSquare(1).getColor());
+		newCube.right.getSquare(8).setColor(newCube.holder.getSquare(2).getColor());
+		
+		newCube.solvePath.Add(11);
+		
+		return newCube;
+	}
 	/*
 	* Returns the Cube's shufflePath list.
 	*/
